ead4523bc1ee5e8af17957c69dffce275f9395eb
Edgar Vergara <edgarvm13@hotmail.com>
2024-08-26 07:42:39 +0200

    ✨ ng on destroy

--- src/app/products/components/price/price.component.ts
+++ src/app/products/components/price/price.component.ts
@@ -6,6 +6,7 @@ import {
   OnInit,
   SimpleChanges,
 } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'products-price',
@@ -16,8 +17,14 @@ export class PriceComponent implements OnInit, OnChanges, OnDestroy {
   @Input()
   public price: number = 0;
 
+  public interval$?: Subscription;
+
   ngOnInit(): void {
     console.log('Child component: ngOnInit');
+
+    this.interval$ = interval(1000).subscribe((value) =>
+      console.log(`Tick: ${value}`)
+    );
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -27,5 +34,6 @@ export class PriceComponent implements OnInit, OnChanges, OnDestroy {
 
   ngOnDestroy(): void {
     console.log('Child component: ngOnDestroy');
+    this.interval$?.unsubscribe();
   }
 }
